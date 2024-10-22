0bd319d0d597bfc54367b31c694cb88f23ae688f
Facu Roly <frr.1991@gmail.com>
2024-10-22 12:24:14 +0200

    Initial commit

---
+++ src/main/java/com/pruebatecnica/kuikoservice/KuikoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pruebatecnica.kuikoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuikoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuikoServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pruebatecnica/kuikoservice/KuikoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebatecnica.kuikoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuikoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
